155f35865bdd90df849d566a8455d74939ae0026
ArioShaman <ariosha6@gmail.com>
2018-01-02 00:25:20 +0300

    simple parsing of music

--- Russia2017.py
+++ Russia2017.py
@@ -16,7 +16,7 @@ url = 'http://www.europaplus.ru/index.php?go=Chart40'
 
 def saveHTML(url,name, headers = headers):
   try:
-    req = requests.get(url,headers = headers)
+    req = requests.get(url, headers = headers)
     print req.encoding
     with open(name, 'w') as f:
       text = req.text.splitlines()
@@ -35,29 +35,56 @@ def writeCSV(List, filename = 'test.csv'):
       writer = csv.writer(csvfile, delimiter=',')
       for row in List:
         try:
-          writer.writerow([row[0], row[1], row[2], row[3]])
+          writer.writerow([row[0], row[1], row[2], row[3], row[4], row[5]])
         except UnicodeEncodeError:
           writer.writerow([row[0].encode('utf-8'), row[1].encode('utf-8'), row[2].encode('utf-8'), row[3].encode('utf-8')])
         finally:
           #print "data view as: ", row[0], ' and ', row[1]  
           pass
 
-def getCover(author, sing):
-  reqStr = author + ' ' sing
+def getAudio(author, sing):
+  reqStr = author + ' ' + sing 
+  reqStr = reqStr.replace('& ','')
+  #print reqStr  
   reqStr = reqStr.replace(' ','+')
+  #print reqStr
+  googurl = 'https://www.google.ru/search?newwindow=1&ei=O41KWrz4J-nX6QTFgbWYDQ&q=%s'%reqStr
+
+  req = requests.get(googurl, headers = headers)
+  response = HR(url=googurl, body = req.text, encoding='utf-8')
+
+  try:
+    links = response.css('.kv ._Rm::text').extract()
+    mp3party = ''
+    for link in links:
+      if 'mp3party.net' in link:
+        mp3party = 'http://' + link
+        break
+    print mp3party 
+    req = requests.get(mp3party,headers = headers)
+    response = HR(url=mp3party, body = req.text, encoding='utf-8')
+    audio = response.css('.jp-play::attr(href)').extract_first()
+    print audio
+  except:
+    audio = ''
+  return audio
+
+def getCover(author, sing):
+  reqStr = author + ' ' + sing
+  reqStr = reqStr.replace(' ','+') 
   coverurl = 'https://www.google.ru/search?q=%s&newwindow=1&client=opera&hs=1DR&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjRus-zrLDYAhXNJVAKHZTsBPQQ_AUICigB&biw=1277&bih=673'%reqStr
 
-  req = requests.get(coverurl,headers = headers)
+  req = requests.get(coverurl, headers = headers)
   response = HR(url=coverurl, body = req.text, encoding='utf-8')
 
   img = response
-  img = str(img.css('.rg_bx .rg_meta::text').extract_first())
+  img = unicode(img.css('.rg_bx .rg_meta::text').extract_first())
   img = ast.literal_eval(img)
   img = img['ou']  
   return img
 
-req = requests.get(url,headers = headers)
-response = HR(url=url, body = req.text,encoding='windows-1251')
+req = requests.get(url, headers = headers)
+response = HR(url=url, body = req.text, encoding='windows-1251')
 
 songs = response.css('.songs-holder > .jp-title').extract()
 
@@ -67,20 +94,13 @@ sings = response.css('.songs-holder > .jp-title > span::text').extract()
 
 List = []
 for i in xrange(len(songs)):
-  List.append([authors[i], sings[i], 'Russia', '2017'])
+  img = getCover(authors[i], sings[i])
+  audio = getAudio(authors[i], sings[i])
+  if audio == None:
+    audio = ''
+  List.append([authors[i], sings[i], img, audio, 'Russia', '2017'])
 
-img = 
-# reqStr = List[0][0] + ' ' + List[0][1]
-# reqStr = reqStr.replace(' ','+')
-# coverurl = 'https://www.google.ru/search?q=%s&newwindow=1&client=opera&hs=1DR&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjRus-zrLDYAhXNJVAKHZTsBPQQ_AUICigB&biw=1277&bih=673'%reqStr
-
-# req = requests.get(coverurl,headers = headers)
-# response = HR(url=coverurl, body = req.text, encoding='utf-8')
-
-# img = response
-# img = str(img.css('.rg_bx .rg_meta::text').extract_first())
-# img = ast.literal_eval(img)
-# img = img['ou']
-#writeCSV(List, filename = 'Russia2017.csv')
+writeCSV(List, filename = 'Russia2017.csv')
 
 #saveHTML(url,'test.html')
+
